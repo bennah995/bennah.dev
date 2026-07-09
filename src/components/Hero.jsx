93a8cffcdd08343c08e51cc16d52d7e51e7dc7d7
Bennah <bennah955@gmail.com>
2026-07-09 23:52:24 +0300

    Made a couple of changes with styling- switched to tailwind css and customized it to fit my style

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+
+
+function HeroSection(){
+  return(
+    <div>
+      <h1>BENARD MUTUKU</h1>
+      <p>Hello, I'm Bennah</p>
+      <span>Software Engineer</span>
+      <p>Building purposeful software that solves real problems, one product at a time.</p>
+      <button>View Work</button>
+      <button>Contact Me</button>
+    </div>
+  )
+}
+
+export default HeroSection;
